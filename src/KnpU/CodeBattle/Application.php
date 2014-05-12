f498bdd7cf20712658f40dd7ccfda9edc81a1da6
Ryan Weaver <ryan@thatsquality.com>
2014-05-12 17:10:39 -0500

    [tuts-apply-diff] Applying existing diff file for step errors-exception-listener-for-apiproblemexception

--- src/KnpU/CodeBattle/Application.php
+++ src/KnpU/CodeBattle/Application.php
@@ -5,6 +5,7 @@ namespace KnpU\CodeBattle;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\CachedReader;
 use Doctrine\Common\Cache\PhpFileCache;
+use KnpU\CodeBattle\Api\ApiProblemException;
 use KnpU\CodeBattle\Battle\PowerManager;
 use KnpU\CodeBattle\Repository\BattleRepository;
 use KnpU\CodeBattle\Repository\ProjectRepository;
@@ -23,6 +24,7 @@ use KnpU\CodeBattle\Repository\UserRepository;
 use KnpU\CodeBattle\Repository\ProgrammerRepository;
 use KnpU\CodeBattle\Battle\BattleManager;
 use Silex\Provider\ValidatorServiceProvider;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Validator\Mapping\ClassMetadataFactory;
 use Symfony\Component\Validator\Mapping\Loader\AnnotationLoader;
 
@@ -215,6 +217,19 @@ class Application extends SilexApplication
 
     private function configureListeners()
     {
-        // todo
+        $this->error(function(\Exception $e, $statusCode) {
+            // only do something special if we have an ApiProblemException!
+            if (!$e instanceof ApiProblemException) {
+                return;
+            }
+
+            $response = new JsonResponse(
+                $e->getApiProblem()->toArray(),
+                $statusCode
+            );
+            $response->headers->set('Content-Type', 'application/problem+json');
+
+            return $response;
+        });
     }
 } 
